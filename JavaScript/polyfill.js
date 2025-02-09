ea7db5dfb54c86ef9fa830d4b09159d09fbd62ae
Saad <saad_eew@hotmail.com>
2025-02-09 21:28:05 +0500

    Add Map Polyfill

---
+++ JavaScript/polyfill.js
@@ -0,0 +1,11 @@
+// -----------Map Polyfill------------------
+// if(!Array.prototype.myMap){
+//     Array.prototype.myMap = function(userFun){
+//         const result = [];
+//         for(let i = 0; i<this.length; i++){
+//             let value= userFun(this[i], i)
+//             result.push(value);
+//         }
+//         return result;  
+//     }
+// }
