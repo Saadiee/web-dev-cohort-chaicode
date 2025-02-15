59e9ae14385c8046249ace7bf6ae8525a65da22e
Saad <saad_eew@hotmail.com>
2025-02-16 01:31:37 +0500

    Add objectClass.js

---
+++ experiments/js-objects/objectClass.js
@@ -0,0 +1,6 @@
+class Person{                           // class is blueprint for object
+    constructor(fname,lname){           // constructor includes all those properties that we want in that object
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
