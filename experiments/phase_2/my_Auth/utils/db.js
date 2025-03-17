5434caf59c1833f6ac25b6caf6ca0a6918cad868
Saad <saad_eew@hotmail.com>
2025-03-17 05:23:29 +0500

    refactor database connection to use a constant for MongoDB URL

--- experiments/phase_2/my_Auth/utils/db.js
+++ experiments/phase_2/my_Auth/utils/db.js
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const mongourl = process.env.CLUSTER_URL
+
 export default function db() {
-    return mongoose.connect(process.env.CLUSTER_URL)
+    return mongoose.connect(mongourl)
     .then(() => console.log("✅ Connected to MongoDB"))
     .catch(err => {
         console.error("❌ Failed to connect to MongoDB", err);
-        process.exit(1); // Stop the app if DB connection fails
     });
 }
