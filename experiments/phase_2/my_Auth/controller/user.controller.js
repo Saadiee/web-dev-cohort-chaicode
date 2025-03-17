d6a1967fb400c63578b3cbb606fda6457bddf1d0
Saad <saad_eew@hotmail.com>
2025-03-17 05:23:07 +0500

    add user profile retrieval, logout, and password reset functionality

--- experiments/phase_2/my_Auth/controller/user.controller.js
+++ experiments/phase_2/my_Auth/controller/user.controller.js
@@ -63,12 +63,12 @@ const registerUser = async(req, res) => {
             from:  process.env.NODEMAILER_USER, // sender address
             to: user.email, // list of receivers
             subject: "Verify email Adress ✔", // Subject line
-            text: `Link - http://localhost:3000/verify/${user.token}`, // plain text body
+            text: `Link - http://localhost:3000/verify/${user.verificationToken}`, // plain text body
             html: `<h1>Verify your email address</h1>
             <p>Click the link below to verify your email address</p>
             <p> Username: ${user.name}</p>
             <p> Email: ${user.email}</p>
-            <a href="${process.env.BASE_URL || 'http://localhost:3000'}/verify/${user.token}">Click this link to verify</a>`, // html body
+            <a href="${process.env.BASE_URL || 'http://localhost:3000'}/verify/${user.verificationToken}">Click this link to verify</a>`, // html body
         }
 
         await transporter.sendMail(mailOptions);
@@ -103,7 +103,7 @@ const verifyUser = async(req, res) => {
     user.isVerified = true;
 
     // rm verification token
-    user.verificationToken = null;
+    user.verificationToken = '';
 
     // save
     await user.save();
@@ -153,4 +153,123 @@ const loginUser = async(req, res) => {
     }
 }
 
-export {registerUser, verifyUser, loginUser}
+const getMe = async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if(!user)
+            {return res.status(400).json({success: false, message: "User not found"})}
+        res.status(200).json({
+            success: true,
+            user
+        })
+    } catch (error) {
+        {return res.status(400).json({success: false, message: "Caught Error in profile"})}
+    }
+}
+
+const logoutUser = async(req, res) => {
+    try {
+        res.cookie('test', '', {expiresIn: new Date(0)})
+        res.status(200).json({
+            success: true,
+            message: "Logged out successfully"
+        })
+    } catch (error) {
+        {return res.status(400).json({success: false, message: "Caught Error in cookies"})}
+    }
+}
+
+const forgotPassword = async(req, res) => {
+    try {
+        const {email} = req.body;
+
+        const user = await User.findOne({email:email})
+    
+        const resetToken = crypto.randomBytes(32).toString('hex');
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordTokenExpiration = Date.now() + 10*60*1000;
+        user.save();
+
+        // sending token via mail
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAILTRAP_HOST,
+            port: process.env.MAILTRAP_PORT,
+            secure: false,
+            auth: {
+            user: process.env.NODEMAILER_USER,
+            pass: process.env.NODEMAILER_PASSWORD
+        }});
+        const mailOptions = {
+            from:  process.env.NODEMAILER_USER, // sender address
+            to: user.email, // list of receivers
+            subject: "Reset Password ✔", // Subject line
+            text: `Link - http://localhost:3000/resetpwd/${user.resetPasswordToken}`, // plain text body
+            html: `<h1>Verify your email address</h1>
+            <p>Click the link below to reset your password</p>
+            <p> Username: ${user.name}</p>
+            <p> Email: ${user.email}</p>
+            <a href="${process.env.BASE_URL || 'http://localhost:3000'}/resetpwd/${user.resetPasswordToken}">Click this link to set password</a>
+            <p>Link expires in ${user.resetPasswordTokenExpiration.toLocaleString()}</p>
+            `, // html body
+        }
+
+        transporter.sendMail(mailOptions);
+
+
+        res.status(200).json({
+            message: "Forgot Password Reset Email Sent Successfully",
+            success: true
+        })
+        
+    } catch (error) {
+     console.log("Password reset error", error)
+     res.status(500).json({
+        successfull:false,
+        message: "Password reset mail failures"
+     })
+
+    }
+}
+
+const resetPassword = async(req, res) => {
+       
+        // find user from that token
+        const {token} = req.params;
+        const {password} = req.body;
+        const {rePassword} = req.body;
+    try {
+        const user = await User.findOne({
+            resetPasswordToken: token,
+            resetPasswordTokenExpiration: {$gt: Date.now()}
+        })
+        if(!user){
+            res.status(400).json({
+                message: "Invalid user"
+            })
+        }
+        if(password !== rePassword){
+            res.json({message: "Password doesn't match"})
+        }
+        if(user.resetPasswordTokenExpiration <= Date.now()){
+            res.json({
+                message: "Timelimit expired"
+            })
+        }
+        user.password = password;
+        user.resetPasswordToken = "";
+        user.resetPasswordTokenExpiration= "";
+        user.save();
+        res.json({
+            message: "password reset successfully"
+        })
+        
+        
+    } catch (error) {
+        console.log("Error in password reset", error)
+        res.json({
+            message: `There is problem in password reseting. ${error}`
+        })
+    }
+}
+
+export {registerUser, verifyUser, loginUser, getMe, logoutUser, forgotPassword, resetPassword}
