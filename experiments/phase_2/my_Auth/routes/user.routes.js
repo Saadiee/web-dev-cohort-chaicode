64dc60f2a8970a4b6ff3ce89db62d5481faf1355
Saad <saad_eew@hotmail.com>
2025-03-17 05:23:20 +0500

    add user profile, logout, and password reset routes

--- experiments/phase_2/my_Auth/routes/user.routes.js
+++ experiments/phase_2/my_Auth/routes/user.routes.js
@@ -7,12 +7,17 @@
 // export default router
 
 import express from "express"
-import { loginUser, registerUser, verifyUser } from "../controller/user.controller.js";
+import { loginUser, registerUser, verifyUser, getMe, logoutUser, forgotPassword, resetPassword } from "../controller/user.controller.js";
+import { isLoggedIn } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post('/register', registerUser)
 router.get('/verify/:token', verifyUser)
 router.post('/login', loginUser)
+router.get('/profile',isLoggedIn, getMe)
+router.get('/logout',isLoggedIn, logoutUser)
+router.post('/forgotpwd', forgotPassword)
+router.post('/resetpwd/:token', resetPassword)
 
 export default router;
